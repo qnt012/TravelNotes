15ee19cecb431b29ed7d43cea76398a54199c513
20170043 <20170043@kumoh.ac.kr>
2020-10-10 00:15:33 +0900

    Add Note Update component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <app-header @openEditor="editorOpen = !editorOpen"></app-header>
-    <app-note-editor v-if="editorOpen" @noteAdded="newNote" @noteDeleted="deleteNote"></app-note-editor>        
+    <app-header @openEditor="editorOpen = !editorOpen, updaterOpen = false" ></app-header>
+    <app-note-editor v-if="editorOpen"  @noteAdded="newNote" @noteDeleted="deleteNote" ></app-note-editor>        
+    <app-note-updater :notesData = notes v-if="updaterOpen == true && editorOpen == false" @noteUpdated="updateNewNote"></app-note-updater>
     <div class="noteContainer">
-        <div v-for="(note, index) in notes" :key="`note-${index}`" class="note" :style="{'background-color': note.theme}">
+        <div v-for="(note, index) in notes" @click="updateNote(note.title, note.text, note.theme, index), editorOpen=false, updaterOpen = !updaterOpen" :key="`note-${index}`" class="note" :style="{'background-color': note.theme}">      
             <div>
                 <span class="delete" @click.prevent="deleteNote(index)"><i class="fas fa-times"></i></span> 
-                <span >{{ note.title}}</span>
+                <span >{{ note.title }}</span>
                 <p class="note-text">{{ note.text }}</p>
             </div>
         </div>
-    </div>
+    </div>   
   </div>
 </template>
 
-<script>
+<script> 
 import NoteEditor from './components/NoteEditor.vue';
+import NoteUpdater from './components/NoteUpdater.vue';
 import Header from './components/Header.vue';
 
 export default {
   name: 'App',
+ 
   data: function() {
     return {
       editorOpen: false,
+      updaterOpen: false,
       notes: [
         {
           title: 'Code',
@@ -38,14 +42,20 @@ export default {
     }
   },
 	computed: {
-		
 	},
-  methods: {   
+  methods: { 
     newNote(title, text, theme) {
       this.notes.push({title: title, text: text, theme: theme});
     },
     deleteNote(index) {
       this.notes.splice(index, 1)
+    },
+    updateNote(title, text, theme, index) {
+      this.notes[this.index] = {title: title, text: text, theme: theme, index : index}
+    },
+    updateNewNote(title, text, theme, index) {
+      this.notes[index] = {title: title, text: text, theme: theme}
+      this.updaterOpen = false
     },
   },
   mounted() {
@@ -62,7 +72,8 @@ export default {
   },
   components: {
     appNoteEditor: NoteEditor,
-    appHeader: Header
+    appHeader: Header,
+    appNoteUpdater: NoteUpdater
   }
 }
 </script>
---
+++ src/components/NoteUpdater.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="note-grid">
+        <div class="note-editor">
+            <input class="title-input" type="text" v-model="inputTitle" placeholder="">
+            <textarea rows="10" v-model="inputText" placeholder=""></textarea>   
+            <div class="note-editor-bottom">
+                <button @click=createUpdate class="fas fa-edit"></button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['notesData'],
+    data() {
+        return {
+            inputTitle: this.notesData[this.index].title,                       
+            inputText: this.notesData[this.index].text,
+            inputTheme: this.notesData[this.index].theme,
+            inputIndex: this.notesData[this.index].index
+        }
+    },   
+    methods: {    
+        createUpdate() {
+            this.$emit('noteUpdated', this.inputTitle, this.inputText, this.inputTheme, this.inputIndex);
+            this.inputTitle = '';
+            this.inputText= '',
+            this.inputTheme = '';
+        },
+    },
+}
+</script>
+
