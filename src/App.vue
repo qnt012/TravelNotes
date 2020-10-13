ecf8ff83cc821a3b19c337c2d5542d982ef2f84c
20170043 <20170043@kumoh.ac.kr>
2020-10-13 13:43:26 +0900

    Add Search component(1)

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
     <app-header @openEditor="editorOpen = !editorOpen, updaterOpen = false" ></app-header>
+    <app-note-search-menu @getKeyword="findKeyword"></app-note-search-menu>
     <app-note-editor v-if="editorOpen"  @noteAdded="newNote" @noteDeleted="deleteNote" ></app-note-editor>        
     <app-note-updater :notesData = notes v-if="updaterOpen == true && editorOpen == false" @noteUpdated="updateNewNote"></app-note-updater>
     <div style="z-index: 1" class="noteContainer">
-        <div v-for="(note, index) in notes" :key="`note-${index}`" class="note" :style="{'background-color': note.theme}">      
+        <div v-for="(note, index) in notes" :key="`note-${index}`" class="note" :style="{'background-color': note.theme, 'display': note.display}">      
             <div @click="selected=index">
                 <span class="update" @click="updateNote(note.title, note.text, note.theme, index, note.date, note.writer), editorOpen=false, updaterOpen = !updaterOpen"><i class="fas fa-edit" id="fa-edit"></i></span>
                 <span class="delete" @click.prevent="deleteNote(index), updaterOpen = true, editorOpen = false"><i class="fas fa-times"></i></span> 
@@ -29,6 +30,7 @@ import Header from "./components/Header.vue";
 import OpenMore from "./components/OpenMore.vue";
 import NoteMenu from "./components/NoteMenu.vue";
 import NoteUpdater from './components/NoteUpdater.vue';
+import NoteSearchMenu from './components/NoteSearchMenu.vue';
 
 export default {
   name: "App",
@@ -37,20 +39,13 @@ export default {
       editorOpen: false,
       updaterOpen: false,
       notes: [],
-      selected: -1
+      selected: -1,
     };
   },
   computed: {},
   methods: {
     newNote(title, text, theme, date, writer) {
-      this.notes.push({
-        title: title,
-        text: text,
-        theme: theme,
-        date: date,
-        writer: writer,
-        moreOpen: false
-      });
+      this.notes.push({title: title, text: text, theme: theme, date: date, writer: writer, moreOpen: false, display: "auto"});
     },
     deleteNote(index) {
       this.notes.splice(index, 1)
@@ -65,7 +60,23 @@ export default {
     reColor(theme) {
       this.notes[this.selected].theme = theme;
       this.notes.moreOpen = false;
-    }
+    },
+    findKeyword(keyword) {
+      for (var i = 0; i < this.notes.length; i++) {
+        if (this.notes[i].title == keyword || keyword =='') {
+            this.notes[i].display = "inline-block";
+          } 
+        else if (this.notes[i].text == keyword || keyword =='') {
+            this.notes[i].display = "inline-block";
+          } 
+        else if (this.notes[i].writer == keyword || keyword =='') {
+            this.notes[i].display = "inline-block";
+          }
+          else {
+            this.notes[i].display = "none";
+          }
+        }
+    },
   },
   mounted() {
     if (localStorage.getItem("notes"))
@@ -85,7 +96,8 @@ export default {
     appHeader: Header,
     appOpenMore: OpenMore,
     appNoteMenu: NoteMenu,
-    appNoteUpdater: NoteUpdater
+    appNoteUpdater: NoteUpdater,
+    appNoteSearchMenu: NoteSearchMenu
   }
 };
 </script>
---
+++ src/components/NoteSearchMenu.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="noteSearchMenu">
+        <input type="search" v-model="keyword" @change="searchKeyword" placeholder="  검색"/><i class="fas fa-search"></i>  
+    </div>
+</template>
+
+<script>
+export default {
+  methods:{
+    searchKeyword() {
+      this.$emit('getKeyword', this.keyword);
+      this.keyword = ''
+    },
+  },
+}
+</script>
