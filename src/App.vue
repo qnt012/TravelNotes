135710775a701f17f63f3bad683bcec63b28cbf6
20170043 <20170043@kumoh.ac.kr>
2020-10-14 01:44:03 +0900

    Update Noteupdate component bug(Note update window was opened by other button)

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="app">
     <app-header @openEditor="editorOpen = !editorOpen, updaterOpen = false" ></app-header>
+    <app-note-search-menu @getKeyword="findKeyword"></app-note-search-menu>
     <app-note-editor v-if="editorOpen"  @noteAdded="newNote" @noteDeleted="deleteNote" ></app-note-editor>        
     <app-note-updater :notesData = notes v-if="updaterOpen == true && editorOpen == false" @noteUpdated="updateNewNote"></app-note-updater>
     <div style="z-index: 1" class="noteContainer">
-        <div v-for="(note, index) in notes" :key="`note-${index}`" class="note" :style="{'background-color': note.theme}">      
+        <div v-for="(note, index) in notes" :key="`note-${index}`" class="note" :style="{'background-color': note.theme, 'display': note.display}">      
             <div @click="selected=index">
-                <span class="update" @click="updateNote(note.title, note.text, note.theme, index, note.date, note.writer), editorOpen=false, updaterOpen = !updaterOpen"><i class="fas fa-edit" id="fa-edit"></i></span>
+                <span class="update" @click="updateNote(note.title, note.text, note.theme, index, note.date, note.writer), editorOpen=false, updaterOpen = !updaterOpen, updaterButton =  !updaterButton"><i v-if="updaterButton" class="fas fa-edit" id="fa-edit"></i></span>
                 <span class="delete" @click.prevent="deleteNote(index), updaterOpen = true, editorOpen = false"><i class="fas fa-times"></i></span> 
                 <app-open-more @openMore="note.moreOpen = !note.moreOpen"></app-open-more>
                 <app-note-menu :notesData="notes" v-if="note.moreOpen" @recolorMenu="reColor"></app-note-menu>
@@ -29,6 +30,7 @@ import Header from "./components/Header.vue";
 import OpenMore from "./components/OpenMore.vue";
 import NoteMenu from "./components/NoteMenu.vue";
 import NoteUpdater from './components/NoteUpdater.vue';
+import NoteSearchMenu from './components/NoteSearchMenu.vue';
 
 export default {
   name: "App",
@@ -36,6 +38,8 @@ export default {
     return {
       editorOpen: false,
       updaterOpen: false,
+      updaterButton: true,
+      updaterCancel:false,
       notes: [],
       selected: -1
     };
@@ -43,14 +47,7 @@ export default {
   computed: {},
   methods: {
     newNote(title, text, theme, date, writer) {
-      this.notes.push({
-        title: title,
-        text: text,
-        theme: theme,
-        date: date,
-        writer: writer,
-        moreOpen: false
-      });
+      this.notes.push({title: title, text: text, theme: theme, date: date, writer: writer, moreOpen: false, display: "auto"});
     },
     deleteNote(index) {
       this.notes.splice(index, 1)
@@ -61,11 +58,32 @@ export default {
     updateNewNote(title, text, theme, index, date, writer) {
       this.notes[index] = {title: title, text: text, theme: theme, date:date, writer:writer}
       this.updaterOpen = false
+      this.updaterButton = true
     },
     reColor(theme) {
       this.notes[this.selected].theme = theme;
       this.notes.moreOpen = false;
-    }
+    },
+    findKeyword(keyword) {
+      for (var i = 0; i < this.notes.length; i++) {
+        this.notes[i].display = "none";
+        if (keyword != ''){
+          if (this.notes[i].title.indexOf(keyword) != -1) {
+            this.notes[i].display = "inline-block";
+          } 
+          if (this.notes[i].text.indexOf(keyword) != -1) {
+            this.notes[i].display = "inline-block";
+          } 
+          if (this.notes[i].writer.indexOf(keyword)!= -1) {
+            this.notes[i].display = "inline-block";
+          }
+        }
+        else {
+          this.notes[i].display = "inline-block";
+        }
+       
+      }
+    },
   },
   mounted() {
     if (localStorage.getItem("notes"))
@@ -85,7 +103,8 @@ export default {
     appHeader: Header,
     appOpenMore: OpenMore,
     appNoteMenu: NoteMenu,
-    appNoteUpdater: NoteUpdater
+    appNoteUpdater: NoteUpdater,
+    appNoteSearchMenu: NoteSearchMenu
   }
 };
 </script>
