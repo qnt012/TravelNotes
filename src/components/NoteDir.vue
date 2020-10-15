cfb26a4dfa646966b23d31a0ada670c070a2571b
20170270 <dgb980228@naver.com>
2020-10-15 15:16:22 +0900

    Add noteDirectory(1)

---
+++ src/components/NoteDir.vue
@@ -0,0 +1,16 @@
+<template>
+  <div style="z-index: 2" class="note-directory">
+    <p>RRRRRRRRRRRRRRRRRRRRRRRRRRR</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["notesData"],
+  data() {
+    return {
+      displayTitle: this.notesData[this.index].title,
+    };
+  },
+};
+</script>
